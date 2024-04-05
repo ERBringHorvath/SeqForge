7ebfccac532f9cecc92c9a715db9ae5bfc650b61
Eli Bring Horvath <97261650+ERBringHorvath@users.noreply.github.com>
2024-04-05 15:56:18 -0600

    Update extract_sequences.py

--- bin/extract_sequences.py
+++ bin/extract_sequences.py
@@ -2,13 +2,22 @@ import os
 from Bio import SeqIO
 import concurrent.futures
 
+"""
+multiBLAST extract
+
+This program is designed to extract sequences based on a multiBLAST query. Translation of sequences is optional, however care
+    should be used when translating extracted sequences, as BLAST results may not always contain a full CDS. To allow for this, when 
+    the --translate argument is called, extracted sequences will be trimmed to only include complete codons, which may affect interpretation
+    of results. 
+"""
+
 def find_fasta_file(basename, fasta_dir):
     for file in os.listdir(fasta_dir):
         if file.startswith(basename) and file.split('.')[-1] in ['fa', 'fas', 'fasta', 'ffn', 'fna']:
             return os.path.join(fasta_dir, file)
     return None
 
-def process_blast_file(blast_file, blast_dir, fasta_dir, evalue_cutoff):
+def process_blast_file(blast_file, blast_dir, fasta_dir, evalue_cutoff, translate=False):
     file_path = os.path.join(blast_dir, blast_file)
     if os.stat(file_path).st_size == 0:
         print(f"Skipping empty file: {blast_file}")
@@ -43,9 +52,17 @@ def process_blast_file(blast_file, blast_dir, fasta_dir, evalue_cutoff):
                             else:  #Negative strand, reverse complement
                                 aligned_sequence = seq_record.seq[send-1:sstart].reverse_complement()
                             
-                            new_record = SeqIO.SeqRecord(aligned_sequence, id=seq_record.id, description="aligned region")
+                            if translate: 
+                                #Trim the sequence to a multiple of 3
+                                trim_length = len(aligned_sequence) % 3
+                                if trim_length > 0:
+                                    aligned_sequence = aligned_sequence[:-trim_length]
+                                
+                                aligned_sequence = aligned_sequence.translate()
+                            
+                            new_record = SeqIO.SeqRecord(aligned_sequence, id=seq_record.id, description="aligned region (translated)" if translate else "aligned region")
                             unique_sequences[seq_id] = new_record
-                            break  #Found the sequence, no need to continue the loop
+                            break
 
     return unique_sequences
 
@@ -56,15 +73,16 @@ def extract_sequences_tabular(args):
     output_fasta = args.output_fasta
     evalue_cutoff = float(args.evalue) if args.evalue else 0.001
     cores = int(args.threads) if args.threads else 1
+    translate = args.translate
 
     blast_files = [file for file in os.listdir(blast_dir) if file.endswith('.txt')]
 
-    print(f"\n \033[92mTotal BLAST files: {len(blast_files)}\033[0m")
+    print(f"\n \033[92mTotal BLAST files: {len(blast_files)}\033[0m\n")
     
     all_unique_sequences = {}
 
     with concurrent.futures.ProcessPoolExecutor(max_workers=cores) as executor:
-        futures = {executor.submit(process_blast_file, blast_file, blast_dir, fasta_dir, evalue_cutoff): blast_file for blast_file in blast_files}
+        futures = {executor.submit(process_blast_file, blast_file, blast_dir, fasta_dir, evalue_cutoff, translate): blast_file for blast_file in blast_files}
 
         for future in concurrent.futures.as_completed(futures):
             unique_sequences = future.result()
