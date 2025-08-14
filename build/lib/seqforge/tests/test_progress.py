32c860de3b066ee9195fc9ea0ab1cc5318532b33
Elijah R. Bring Horvath, PhD <97261650+ERBringHorvath@users.noreply.github.com>
2025-08-14 10:43:03 -0600

    Delete build/lib/seqforge directory

--- build/lib/seqforge/tests/test_progress.py
+++
@@ -1,7 +0,0 @@
-from seqforge.utils.progress import ProgressHandler
-
-def test_progress_basic():
-    ph = ProgressHandler(total=3, prefix="T", mode="bar")
-    ph.update(1)
-    ph.update(2)
-    ph.finish()
